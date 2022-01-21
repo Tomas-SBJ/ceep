600adb7acbe2b2e82b185d56a6d237bdd4751b32
TomasSbj <tomassbj@gmail.com>
2022-01-21 13:31:59 -0300

    Criaçao de componentes

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import CardNota from './CardNota';
+
+class ListaDeNotas extends Component {
+
+    render() {
+        return (
+            <ul>
+                {Array.of("Trabalho", "Trabalho", "Estudos").map(categorias => {
+                    return (
+                        <li>
+                            <div>{categorias}</div>
+                            <CardNota />
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+}
+
+export default ListaDeNotas;
